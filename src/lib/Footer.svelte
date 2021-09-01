3c2266eafaf8c22df9df5f390348d3cfcdd3f629
Puru Vijay <devpuruvj@gmail.com>
2021-09-02 00:30:26 +0530

    Update contacts stuff

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -5,17 +5,34 @@
         <p>Contact Us:</p>
 
         <p>VYSHNAV K</p>
-        <p>mfd19i016@iiitdm.ac.in</p>
+        <p>infernodragsters.iiitdm@gmail.com</p>
         <p>+91 9447138708</p>
 
         <br />
 
         <p>Follow us on:</p>
         <p>
-          <a href="https://instagram.com/inferno_dragsters_iiitdm?utm_medium=copy_link">
+          <a
+            target="_blank"
+            rel="noopener"
+            href="https://instagram.com/inferno_dragsters_iiitdm?utm_medium=copy_link"
+          >
             Instagram
           </a>
-          <a href="https://www.linkedin.com/company/inferno-dragsters/">Linkedin</a>
+          <a
+            target="_blank"
+            rel="noopener"
+            href="https://www.linkedin.com/company/inferno-dragsters/"
+          >
+            Linkedin
+          </a>
+          <a
+            target="_blank"
+            rel="noopener"
+            href="https://www.facebook.com/Inferno-Dragsters-iiitdm-104021848677545/"
+          >
+            Facebook
+          </a>
         </p>
 
         <br />
